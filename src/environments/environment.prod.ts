d9f617daace26d4661854647ea04b606d71463e2
nydi <stefan.nydegger@gmail.com>
2018-03-26 23:33:53 +0200

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB3lowhGejQjmQNRV6bXu5VPsJocRI0sJA',
+    authDomain: 'angularfirebase-666.firebaseapp.com',
+    databaseURL: 'https://angularfirebase-666.firebaseio.com',
+    projectId: 'angularfirebase-666',
+    storageBucket: 'angularfirebase-666.appspot.com',
+    messagingSenderId: '879272462161'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,14 @@
 
 export const environment = {
   production: false,
-  firebase:{
-  	apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+  firebase: {
+    apiKey: 'AIzaSyB3lowhGejQjmQNRV6bXu5VPsJocRI0sJA',
+    authDomain: 'angularfirebase-666.firebaseapp.com',
+    databaseURL: 'https://angularfirebase-666.firebaseio.com',
+    projectId: 'angularfirebase-666',
+    storageBucket: 'angularfirebase-666.appspot.com',
+    messagingSenderId: '879272462161'
   }
 };
+
+
